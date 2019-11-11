076e7c9cd620cd71566ebbbe2ae2d1a5a921a584
bifferos <bifferos@gmail.com>
2019-11-11 15:55:27 +0000

    Run cleanup code

---
+++ biffpkg_sample/install/doinst.sh
@@ -0,0 +1,10 @@
+# Example doinst.sh containing removal code
+
+# This is the step from doinst.sh that runs on install:
+echo "This is installpkg performing some action"
+
+# This code will execute on removal of the package:
+#execute_on_removepkg: echo "This is removepkg performing some action"
+
+
+
